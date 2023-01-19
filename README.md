cc32eb1df2872a14a43630413b3f1ffa5baf9bee
Brandon Ha <idevbrandon@gmail.com>
2023-01-19 19:13:19 +0900

    FEAT add brief home

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # 🔱Reject Modernity, Embrace Masculinity(RMEM)💪
 
 ✅ We go jim!
+
+- gyms nearby you.
+- Earn more than you spend daily.
+- Buy stocks until we conquer the market.
+
+Let's say you have 6 tasks to do.
+It returns a list of messages depending on the progress
+
+```javascript
+let progress = 0;
+switch (progress) {
+  case progress <= 20:
+    return "Get up you lazy baster";
+  case progress <= 50:
+    return "There is no joy without pain by Andraw Tate"
+  case progress <= 100:
+    return "We're All Gonna Make It Brah"
+}
+```
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -15,6 +12,24 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <h1>Reject Modernity, Embrace Masculinity</h1>
+        <div
+          style={{
+            position: "relative",
+            width: "200px",
+            paddingBottom: "100px",
+            left: "50%",
+            transform: "translateX(-50%)",
+          }}
+        >
+          <Image
+            src="/images/zyzz-pose.jpg"
+            alt="zyzz"
+            layout="fill"
+            objectFit="contain"
+            priority
+          />
+        </div>
         <div>Get up you lazy</div>
         <p>Yeah buddy, light weight baby</p>
       </main>
